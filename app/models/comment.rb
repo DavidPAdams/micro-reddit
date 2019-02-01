806eb082e716dc469c6c652b004a0320feb8d55b
David Adams <david.riding.rails@gmail.com>
2019-01-31 18:38:39 -0700

    Finish validations

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,6 +1,8 @@
 class Comment < ApplicationRecord
   validates :title, presence: true, length: { in: 1..20 }
   validates :body, presence: true, length: { in: 1..300 }
+  validates :user_id, numericality: true
+  validates :article_id, numericality: true
   belongs_to :user, dependent: :destroy
   belongs_to :article, dependent: :destroy
 end
